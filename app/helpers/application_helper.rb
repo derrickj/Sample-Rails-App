1681fafad716875e5bd9f99fa28d735e98f670b0
Derrick Jones <derrickjones323@gmail.com>
2011-09-23 14:18:35 -0700

    use helper for logo instead of inside header
    listing 5.32

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+  def logo
+    logo = image_tag ("logo.png", :alt => "Sample App", :class => "round")
+    link_to logo, root_path
+  end
+  
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
     if @title.nil?
