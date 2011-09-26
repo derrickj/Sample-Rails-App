36dbd9c514721a550098fd3ee5a4a983a23507cf
Derrick Jones <derrickjones323@gmail.com>
2011-09-26 12:51:51 -0700

    be_long is not a method, be_valid is
    code that reads like english makes me a bit too comfortable

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -78,7 +78,7 @@ describe User do
   it "should reject long passwords" do
     long = "a" * 41
     hash = @attr.merge(:password => long, :password_confirmation => long)
-    User.new(hash).should_not be_long
+    User.new(hash).should_not be_valid
   end
   
   describe "password encryption" do
