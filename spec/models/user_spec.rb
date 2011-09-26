8789058e2b1080e7e62ec4056b32d07baed1b6d9
Derrick Jones <derrickjones323@gmail.com>
2011-09-26 12:46:01 -0700

    start working on passwords for users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,12 @@ require 'spec_helper'
 describe User do
   
   before(:each) do
-    @attr = { :name => "Example User", :email => "user@example.com"}
+    @attr = {
+        :name => "Example User",
+        :email => "user@example.com",
+        :password => "foobar",
+        :password_confirmation => "foobar"
+    }
   end
   
   it "should create a new instance given valid attributes" do
@@ -55,4 +60,34 @@ describe User do
     user_with_duplicate_email.should_not be_valid
   end
   
+  it "should require a password" do
+    User.new(@attr.merge(:password => "", :password_confirmation => "")).
+      should_not be_valid
+  end
+  
+  it "should require matching password confirmation" do
+    User.new(@attr.merge(:password_confirmation =>"invalid"))
+  end
+  
+  it "should reject short passwords" do
+    short = "a"*5
+    has = @attr.merge(:password => short, :password_confirmation => short)
+    User.new(hash).should_not be_valid
+  end
+  
+  it "should reject long passwords" do
+    long = "a" * 41
+    hash = @attr.merge(:password => long, :password_confirmation => long)
+    User.new(hash).should_not be_long
+  end
+  
+  describe "password encryption" do
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
+  end
 end
